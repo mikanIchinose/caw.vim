bb575eb22dd2288122b6c0ce1eb62fd23280eace
Ichinose Yoshihiro <maruisansmai@gmail.com>
2022-12-28 03:33:34 +0900

    fix: error from treesitter

--- lua/caw.lua
+++ lua/caw.lua
@@ -1,37 +1,16 @@
-local languages = {
-  cs = 'c_sharp',
-  javascriptreact = 'jsx',
-  sh = 'bash',
-  typescriptreact = 'tsx',
-}
-
 local M = {}
 
 function M.has_syntax(lnum, col)
-  local col = col - 1
-  local bufnr = vim.api.nvim_get_current_buf()
-  local filetype = vim.api.nvim_buf_get_option(bufnr, 'ft')
-  local lang = languages[filetype] or filetype
-  if not require"vim.treesitter.language".require_language(lang, nil, true) then
-    return false
-  end
-  local query = require"vim.treesitter.query".get_query(lang, "highlights")
-  local tstree = vim.treesitter.get_parser(bufnr, lang):parse()[1]
-  local tsnode = tstree:root()
+	local bufnr = vim.api.nvim_get_current_buf()
+	for _, capture in pairs(vim.treesitter.get_captures_at_pos(bufnr, lnum - 1, col - 1)) do
+		for _, cap in pairs(capture) do
+			if string.match(cap, "comment") then
+				return true
+			end
+		end
+	end
 
-  for _, match in query:iter_matches(tsnode, bufnr, lnum - 1, lnum) do
-    for id, node in pairs(match) do
-      local _, start_col, _, end_col = node:range()
-      local name = query.captures[id]
-      local highlight = vim.treesitter.highlighter.hl_map[name]
-
-      if col >= start_col and col < end_col and string.match(highlight, 'Comment') then
-        return true
-      end
-    end
-  end
-
-  return false
+	return false
 end
 
 return M
